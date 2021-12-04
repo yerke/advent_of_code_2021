79619c57f43dbca0d4318f28fe59e16f6c308f31
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2021-12-03 20:21:55 -0800

    day 2-2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -6,7 +6,11 @@ fn main() -> Result<()> {
     let file = BufReader::new(File::open("data/day02.txt")?);
 
     let mut x = 0;
-    let mut y = 0;
+    let mut y1 = 0;
+
+    let mut aim = 0;
+    let mut y2 = 0;
+
     for line in file.lines() {
         let line = line?;
 
@@ -17,14 +21,24 @@ fn main() -> Result<()> {
         let units: u32 = parts[1].parse()?;
 
         match command {
-            "forward" => x += units,
-            "down" => y += units,
-            "up" => y -= units,
+            "forward" => {
+                x += units;
+                y2 += aim * units;
+            }
+            "down" => {
+                y1 += units;
+                aim += units;
+            }
+            "up" => {
+                y1 -= units;
+                aim -= units;
+            }
             _ => panic!("Unexpected command: {}", command),
         }
     }
 
-    println!("X: {}, Y: {}, product: {}", x, y, x * y);
+    println!("Part 1: X: {}, Y: {}, product: {}", x, y1, x * y1);
+    println!("Part 2: X: {}, Y: {}, product: {}", x, y2, x * y2);
 
     Ok(())
 }
