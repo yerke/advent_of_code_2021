a04e605942810840494c3bac6b28ea7839daddd6
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2021-12-07 11:35:39 -0800

    day 7-2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -17,25 +17,34 @@ fn main() -> Result<()> {
     let min_position = *positions.iter().min().unwrap();
     let max_position = *positions.iter().max().unwrap();
 
-    let mut lowest_cost = None;
+    let mut lowest_cost1 = None;
+    let mut lowest_cost2 = None;
     for p in min_position..max_position {
-        let mut cost = 0;
+        let mut cost1 = 0;
+        let mut cost2 = 0;
+
         for e in &positions {
-            cost += (e - p).abs();
+            let diff = (e - p).abs();
+            cost1 += diff;
+            cost2 += (diff + 1) * diff / 2;
         }
 
-        // println!("p {}, cost {}", p, cost);
-
-        if lowest_cost.is_none() {
-            lowest_cost = Some(cost);
+        if lowest_cost1.is_none() {
+            lowest_cost1 = Some(cost1);
+            lowest_cost2 = Some(cost2);
         }
 
-        if lowest_cost.unwrap() > cost {
-            lowest_cost = Some(cost);
+        if lowest_cost1.unwrap() > cost1 {
+            lowest_cost1 = Some(cost1);
+        }
+
+        if lowest_cost2.unwrap() > cost2 {
+            lowest_cost2 = Some(cost2);
         }
     }
 
-    println!("Part 1: {}", lowest_cost.unwrap());
+    println!("Part 1: {}", lowest_cost1.unwrap());
+    println!("Part 2: {}", lowest_cost2.unwrap());
 
     Ok(())
 }
