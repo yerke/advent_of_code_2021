c0e9021ba28caf9ff46a5039a4be3910ef49455a
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2021-12-07 21:24:02 -0800

    day 8-1

---
+++ src/bin/day08.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<()> {
+    let file = BufReader::new(File::open("data/day08.txt")?);
+
+    let mut counter = 0;
+
+    for line in file.lines() {
+        let line = line?;
+
+        let parts: Vec<&str> = line.split(" | ").collect();
+        let right_part = parts[1];
+
+        let segments: Vec<&str> = right_part.split_whitespace().collect();
+
+        let unique_lengths: [usize; 4] = [2, 4, 3, 7];
+
+        for segment in segments {
+            if unique_lengths.contains(&segment.len()) {
+                counter += 1;
+            }
+        }
+    }
+
+    println!("Part 1: {}", counter);
+
+    Ok(())
+}
