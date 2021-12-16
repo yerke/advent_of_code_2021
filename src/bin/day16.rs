6641dc0f4f8b50af81165e90a9d19163e8d54334
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2021-12-15 22:57:23 -0800

    consume_literal_value

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+const LITERAL_VALUE_TYPE_ID: u64 = 4;
+
 fn main() -> Result<()> {
     let file = BufReader::new(File::open("data/day16.txt")?);
 
@@ -22,11 +24,8 @@ fn main() -> Result<()> {
 
     let mut iter = bits.into_iter();
 
-    let version = read_3_bits(iter.by_ref());
-    dbg!(version);
-
-    let type_id = read_3_bits(iter.by_ref());
-    dbg!(type_id);
+    let version_counter = consume_packet(iter.by_ref());
+    println!("Part 1: sum of versions: {}", version_counter);
 
     Ok(())
 }
@@ -53,18 +52,58 @@ fn hex_to_bits(hex: u8) -> Vec<u8> {
     }
 }
 
-fn read_3_bits(data: impl Iterator<Item = u8>) -> u8 {
-    let bits: Vec<u8> = data.into_iter().take(3).collect();
-    assert_eq!(bits.len(), 3);
-
-    bits_to_u8(&bits)
+fn read_3_bits(mut data: impl Iterator<Item = u8>) -> u64 {
+    let bits: Vec<u8> = get_n_bits(data.by_ref(), 3);
+    bits_to_u64(&bits)
 }
 
-fn bits_to_u8(data: &[u8]) -> u8 {
+fn bits_to_u64(data: &[u8]) -> u64 {
     let mut result = 0;
     for &e in data {
-        result = result * 2 + e;
+        result = result * 2 + e as u64;
     }
 
     result
+}
+
+fn consume_packet(mut data: impl Iterator<Item = u8>) -> u64 {
+    let mut version_counter = 0;
+
+    let version = read_3_bits(data.by_ref());
+    dbg!(version);
+    version_counter += version;
+
+    let type_id = read_3_bits(data.by_ref());
+    dbg!(type_id);
+
+    match type_id {
+        LITERAL_VALUE_TYPE_ID => {
+            let result = consume_literal_value(data.by_ref());
+        }
+        _ => todo!(),
+    }
+
+    version_counter
+}
+
+fn consume_literal_value(mut data: impl Iterator<Item = u8>) -> u64 {
+    let mut result: Vec<u8> = Vec::new();
+    loop {
+        let not_last_group = get_n_bits(data.by_ref(), 1)[0] == 1;
+        let mut bits = get_n_bits(data.by_ref(), 4);
+        result.append(&mut bits);
+
+        if !not_last_group {
+            break;
+        }
+    }
+
+    dbg!(bits_to_u64(&result))
+}
+
+fn get_n_bits(data: impl Iterator<Item = u8>, n: usize) -> Vec<u8> {
+    let bits: Vec<u8> = data.into_iter().take(n).collect();
+    assert_eq!(bits.len(), n);
+
+    bits
 }
